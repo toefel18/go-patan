a52052338c8eaed65abc8a9d88084a80aaa0110a
christophe.o.hesters <toefel18@gmail.com>
2016-09-24 19:31:39 +0200

    Updated the API so that everything is an interface and the implementation can specify the concrete types

--- statistics/api.go
+++ statistics/api.go
@@ -18,71 +18,71 @@
 package statistics
 
 type Stopwatch interface {
-    ElapsedMillis() int64
-    ElapsedNanos() int64
+	ElapsedMillis() int64
+	ElapsedNanos() int64
 }
 
 type Distribution interface {
-    GetSampleCount() int64
-    GetMinimum() int64
-    GetMaximum() int64
-    GetSampleAverage() float64
-    GetSampleVariance() float64
-    GetSampleStdDeviation() float64
+	GetSampleCount() int64
+	GetMinimum() int64
+	GetMaximum() int64
+	GetSampleAverage() float64
+	GetSampleVariance() float64
+	GetSampleStdDeviation() float64
 }
 
-type Snapshot struct {
-    TimestampTaken int64
-    Durations      map[string]Distribution `json:"durations"`
-    Counters       map[string]int64        `json:"counters"`
-    Samples        map[string]Distribution `json:"samples"`
+type Snapshot interface {
+	GetTimestampTaken() int64
+	GetDurations() map[string]Distribution
+	GetCounters() map[string]int64
+	GetSamples() map[string]Distribution
 }
 
 type Store interface {
-    StartStopwatch() Stopwatch
+	StartStopwatch() Stopwatch
 
-    // Finds a duration and returns (true, duration distribution) if found else (false, distribution with all fields set to 0)
-    FindDuration(key string) (bool, Distribution)
+	// Finds a duration and returns (true, duration distribution) if found else (false, distribution with all fields set to 0)
+	FindDuration(key string) (bool, Distribution)
 
-    // Finds a counter and returns (true, counter value) if found, else (false, 0)
-    FindCounter(key string) (bool, int64)
+	// Finds a counter and returns (true, counter value) if found, else (false, 0)
+	FindCounter(key string) (bool, int64)
 
-    // Finds a sample and returns (true, sample distribution) if found else (false, distribution with all fields set to 0)
-    FindSample(key string) (bool, Distribution)
+	// Finds a sample and returns (true, sample distribution) if found else (false, distribution with all fields set to 0)
+	FindSample(key string) (bool, Distribution)
 
-    // Records the elapsed time of the stopwatch and adds that to the distribution identified by key.
-    // Returns the recorded millis
-    RecordElapsedTime(key string, stopwatch Stopwatch) int64
+	// Records the elapsed time of the stopwatch and adds that to the distribution identified by key.
+	// Returns the recorded millis
+	RecordElapsedTime(key string, stopwatch Stopwatch) int64
 
-    // Records duration of the subject function and adds that to the distribution identified by key.
-    // Returns the recorded millis
-    MeasureFunc(key string, subject func()) int64
+	// Records duration of the subject function and adds that to the distribution identified by key.
+	// Returns the recorded millis
+	MeasureFunc(key string, subject func()) int64
 
-    // Records duration of the subject function and adds that to the distribution identified by key.
-    // Returns the recorded millis and the returned value of the subject function
-    MeasureFuncWithReturn(key string, subject func() interface{}) (int64, interface{})
+	// Records duration of the subject function and adds that to the distribution identified by key.
+	// Returns the recorded millis and the returned value of the subject function
+	MeasureFuncWithReturn(key string, subject func() interface{}) (int64, interface{})
 
-    // Increments the counter identified with key by 1. If the counter does not yet exist, it will be created
-    // with initial value of 1
-    IncrementCounter(key string)
+	// Increments the counter identified with key by 1. If the counter does not yet exist, it will be created
+	// with initial value of 1
+	IncrementCounter(key string)
 
-    // Decrements the counter identified with key by 1. If the counter does not yet exist, it will be created
-    // with initial value of -1
-    DecrementCounter(key string)
+	// Decrements the counter identified with key by 1. If the counter does not yet exist, it will be created
+	// with initial value of -1
+	DecrementCounter(key string)
 
-    // Adds value to the counter identified with key with key, if the counter does not yet exist, it will be created
-    // and initialized to value. Value can be negative.
-    AddToCounter(key string, value int)
+	// Adds value to the counter identified with key with key, if the counter does not yet exist, it will be created
+	// and initialized to value. Value can be negative.
+	AddToCounter(key string, value int)
 
-    // Adds a value to the sample distribution identified by key. If the distribution does not yet exist, value will be it's initial value.
-    AddSample(key string, value int64)
+	// Adds a value to the sample distribution identified by key. If the distribution does not yet exist, value will be it's initial value.
+	AddSample(key string, value int64)
 
-    // Clears all durations, counters and samples
-    Reset()
+	// Clears all durations, counters and samples
+	Reset()
 
-    // Creates a snapshot containing all currently registered durations, counters and samples
-    Snapshot() Snapshot
+	// Creates a snapshot containing all currently registered durations, counters and samples
+	Snapshot() Snapshot
 
-    // Creates a snapshot and then calls Reset()
-    SnapshotAndReset() Snapshot
+	// Creates a snapshot and then calls Reset()
+	SnapshotAndReset() Snapshot
 }
