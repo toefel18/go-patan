6ea0dee56f8881381333b479d4db962ee97bc6f4
Christophe Hesters <christophe.o.hesters@cgi.com>
2016-10-03 19:49:40 +0200

    added copyright to files where it was missing

--- gopatanbench/benchmark.go
+++ gopatanbench/benchmark.go
@@ -1,3 +1,20 @@
+/*
+ *
+ *     Copyright 2016 Christophe Hesters
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ *
+ */
 package main
 
 import (
@@ -37,13 +54,13 @@ func Benchmrk(threads int64, itemsPerThread int64) {
     for i := int64(0); i < threads; i++ {
         wg.Add(1)
         go func() {
-            defer wg.Done()
             sw := subject.StartStopwatch()
-            defer subject.RecordElapsedTime("goroutine.duration", sw)
             for i := int64(0); i < itemsPerThread; i++ {
                 subject.IncrementCounter("concurrency.counter")
                 subject.AddSample("concurrency.sample", i)
             }
+            subject.RecordElapsedTime("goroutine.duration", sw)
+            wg.Done()
         }()
     }
     wg.Wait()
