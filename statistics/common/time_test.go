019678217bcdd5c252969ad743b91bf757585163
Christophe Hesters <christophe.o.hesters@cgi.com>
2016-12-04 13:17:36 +0100

    Deleted statistics packages

--- statistics/common/time_test.go
+++
@@ -1,9 +0,0 @@
-package common
-
-import "testing"
-
-func TestCurrentTimeMillis(t *testing.T) {
-	if CurrentTimeMillis() < 1480805318583 {
-		t.Errorf("CurrentTimeMillis gave %v, but should be higher than 1480805318583 which was on 2016-12-03", CurrentTimeMillis())
-	}
-}
